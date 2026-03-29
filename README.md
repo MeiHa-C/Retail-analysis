aacc2588062af99cbccb75d3ecec92298a9dcddf
Mei Chen <meiheyo80@gmail.com>
2026-03-29 00:49:06 +0000

    DAY-3 Standardise the product description

--- README.md
+++ README.md
@@ -9,6 +9,13 @@ Online Retail Dataset
 ## 🔧 Tools
 SQL, Python, Power BI
 
+
+## 👉 Data Quality Handling
+### Inconsistent Product Descriptions
+It was observed that the same product (StockCode) is associated with multiple descriptions due to manual entry or formatting inconsistencies.
+To ensure accurate product-level analysis, descriptions were standardised by assigning the most frequent label for each StockCode.
+
+
 ## 🔍 Key Insights
 
 
--- retail.py
+++ retail.py
@@ -3,8 +3,6 @@ import pandas as pd
 df = pd.read_csv("./data/online_retail_II.csv")
 
 
-
-
 # print('\n-----Head-----')
 # print(df.head())
 # print('\n-----Info-----')
@@ -18,7 +16,6 @@ df = pd.read_csv("./data/online_retail_II.csv")
 # print('\n-----Dtypes-----')
 # print(df.dtypes)
 
-
 # print('\n-----isnull-----')
 # print(df.isnull().sum())
 # print('\n-----duplicated-----')
@@ -28,58 +25,121 @@ df = pd.read_csv("./data/online_retail_II.csv")
 # print('\n-----Price <0-----')
 # print(df[df['Price'] <= 0])
 
-# EDA
-## 1. Data Structure
 
-print('===EDA'+'='*50)
-print('\n[1] Basic Info')
-print(f'Rows count: {df.shape[0]:,}  Columns count: {df.shape[1]}')
+def strutureAndQuality(df):
+    """
+    EDA: Explore data
+    """
+    ## 1. Data Structure
+    print('===EDA'+'='*50)
+    print('\n[1] Basic Info')
+    print(f'Rows count: {df.shape[0]:,}  Columns count: {df.shape[1]}')
+
+    ## 2. Data Quality
+    print('\n[2] Missing Values')
+    missing_count = df.isnull().sum()
+    missing_percent = (missing_count / len(df) * 100).round(2)
+    missing_df = pd.DataFrame({'Missing Count': missing_count, 'Percentage(%)': missing_percent})
+    # missing_df = missing_df[missing_df['Missing Count']>0].sort_values(by='Missing Count', ascending=False)
+    missing_df = missing_df.sort_values(by='Missing Count', ascending=False)
+    if not missing_df.empty:
+        print(missing_df)
+    else:
+        print('No missing value.')
+
+    ## 3. Duplicates
+    print('\n[3] Duplicates check')
+    dup_count = df.duplicated().sum()
+    dup_percent = (dup_count / len(df) * 100)
+    print(f'Duplicate count: {dup_count:,} ({dup_percent:.2f}%)')
+
+    ## 4. Anomalies
+    print('\n[4] Business Anomalies')
+
+    # Quantity
+    print(f' ▶ Negative Quantity count (return or cancel): {len(df[df['Quantity'] < 0])}')
+    print(f' ▶ 0 Quantity count: {len(df[df['Quantity'] == 0])}')
+
+    # Price
+    print(f' ▶ Negative Price count (return or cancel): {len(df[df['Price'] < 0])}')
+    print(f' ▶ 0 Price count: {len(df[df['Price'] == 0])}')
+
+    # Invoice
+    invoice_cancel = len(df[df['Invoice'].astype(str).str.startswith('C', na=False)])
+    print(f' ▶ Invoice starts with C count(canceled order):{invoice_cancel:,}')
+
+    ## 5. Consistency 類別一致性檢查
+    print('\n[5] Consistency')
+    country_list = df['Country'].drop_duplicates().sort_values().to_list()
+    # country_list = sorted(df['Country'].unique())
+    # print(f'Country list: {country_list}')
+
+    country_count = len(country_list)
+    # country_count = df['Country'].nunique()
+    print(f'Country count: {country_count}')    
+
+    print('\n\n===Result'+'='*50)
+    print('1. Change missing Customer ID as "Guest"')
+    print('2. Remove duplicates.')
+    print('3. Take negative or zero Quantity and Price away, or maybe do a seperate analysis.')
+
+    # 6. Data Distribution
+    print('\n[6] Distribution')
+    top_product = df.groupby(['Description', 'StockCode'])['Quantity'].sum().reset_index().sort_values(by='Quantity', ascending=False).head(10)
+    print('Top sale product(multiply Qty):')
+    print(top_product)
+    
+    top_freq = df['StockCode'].value_counts().head(10)
+    print(f'\nTop frequent product(by Stock code):\n {top_freq}')
+# ===========================================
+# Find: Description and StockCode don't match
+# 3465     PACK OF 72 RETROSPOT CAKE CASES     21212     49344
+# 3464    PACK OF 72 RETRO SPOT CAKE CASES     21212     46106
+# *** INSIGHT: Inconsistent product descriptions were identified for the same StockCode, which could lead to inaccurate product-level analysis if not standardised.
+# ===========================================
 
 
-## 2. Data Quality
-print('\n[2] Missing Values')
-missing_count = df.isnull().sum()
-missing_percent = (missing_count / len(df) * 100).round(2)
-missing_df = pd.DataFrame({'Missing Count': missing_count, 'Percentage(%)': missing_percent})
-# missing_df = missing_df[missing_df['Missing Count']>0].sort_values(by='Missing Count', ascending=False)
-missing_df = missing_df.sort_values(by='Missing Count', ascending=False)
-if not missing_df.empty:
-    print(missing_df)
-else:
-    print('No missing value.')
 
-## 3. Duplicates
-print('\n[3] Duplicates check')
-dup_count = df.duplicated().sum()
-dup_percent = (dup_count / len(df) * 100)
-print(f'Duplicate count: {dup_count:,} ({dup_percent:.2f}%)')
 
-## 4. Anomalies
-print('\n[4] Business Anomalies')
+def standardise_desc(df):
+    """
+    Product Description Check, Master data quality issue.
 
-# Quantity
-print(f' ▶ Negative Quantity count (return or cancel): {len(df[df['Quantity'] < 0])}')
-print(f' ▶ 0 Quantity count: {len(df[df['Quantity'] == 0])}')
+    Issue: Same StockCode is linked to multiple descriptions, e.g. "RETROSPOT" vs "RETRO SPOT"
+    Solution: Standardise description using most frequent value per StockCode   
 
-# Price
-print(f' ▶ Negative Price count (return or cancel): {len(df[df['Price'] < 0])}')
-print(f' ▶ 0 Price count: {len(df[df['Price'] == 0])}')
+    Args:
+        df: The raw retail dataframe.
 
-# Invoice
-invoice_cancel = len(df[df['Invoice'].astype(str).str.startswith('C', na=False)])
-print(f' ▶ Invoice starts with C count(canceled order):{invoice_cancel:,}')
+    Returns:
+        df_clean (pd.DataFrame)
+    """
 
-## 5. Consistency 類別一致性檢查
-print('\n[5] Consistency')
-country_list = df['Country'].drop_duplicates().sort_values().to_list()
-# country_list = sorted(df['Country'].unique())
-# print(f'Country list: {country_list}')
+    df_clean = df.copy()
 
-country_count = len(country_list)
-# country_count = df['Country'].nunique()
-print(f'Country count: {country_count}')    
+    # Uppercase, remove space
+    df_clean['Description'] = (df_clean['Description']
+                               .astype(str)
+                               .str.strip()
+                               .str.upper()
+                               .str.replace(r'\s+', ' ', regex=True))
 
-print('\n\n===Result'+'='*50)
-print('1. Change missing Customer ID as "Guest"')
-print('2. Remove duplicates.')
-print('3. Take negative or zero Quantity and Price away, or maybe do a seperate analysis.')
+    # Calculate StockCode+Description occur. Sort by counts, only remain the most frequent
+    desc_counts = df_clean.groupby(['StockCode', 'Description']).size().reset_index(name='Count')
+    desc_counts = desc_counts.sort_values(['StockCode', 'Count'], ascending=[True, False])
+    desc_counts = desc_counts.drop_duplicates(subset=['StockCode'], keep='first')
+    
+    # Build a map, mapping back to the df
+    desc_mapping = dict(zip(desc_counts['StockCode'], desc_counts['Description']))
+    df_clean['Description'] = df_clean['StockCode'].map(desc_mapping)
+
+    print(f'\n Descriptions counts(Master Descriptions): {len(desc_counts)}')
+    print('Top 15 most frequent descriptions:')
+    print(desc_counts.sort_values(by='Count', ascending=False).head(15))
+
+    return df_clean
+
+
+
+df_clean = standardise_desc(df)
+# strutureAndQuality(df)
