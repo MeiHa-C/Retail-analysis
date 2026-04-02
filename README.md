2ef3881fc251b0774ecff97544579e53589340fa
Mei Chen <meiheyo80@gmail.com>
2026-04-02 22:02:43 +0100

    DAY-3~5: New features, plotting transactions analysis

--- README.md
+++ README.md
@@ -1,27 +1,43 @@
 # Retail-analysis
 
-## 📌 Business Problem
+## Business Problem
 Understand customer behaviour and improve retention.
 
-## 📊 Dataset
+## Dataset
 Online Retail Dataset
 
-## 🔧 Tools
-SQL, Python, Power BI
+## Tools
+Python
 
 
-## 👉 Data Quality Handling
+## Data Quality Handling
 ### Inconsistent Product Descriptions
-It was observed that the same product (StockCode) is associated with multiple descriptions due to manual entry or formatting inconsistencies.
+It was observed that the same product (StockCode) is associated with multiple descriptions due to manual entry or formatting inconsistencies (e.g. spacing, formatting).
 To ensure accurate product-level analysis, descriptions were standardised by assigning the most frequent label for each StockCode.
 
 
-## 🔍 Key Insights
+## Analysis Focus
+1. When does transaction activity peak? (hourly / weekday patterns)
+2. Customer contribution (revenue)
+3. Top sales
+4. Do customers repeat purchases?
 
 
-## 💡 Business Recommendations
+## Key Insights
+### 1. Transaction Patterns
+Transaction volume peaks in midday between 12–15:00, after 17:00, activity declines. Suggesting reducing engagement in the early morning and evening.
+Weekday results are better than weekend, and Friday indicates worse performance than other weekdays.
 
 
-## 📊 Dashboard
 
 
+## Business Impact
+### 1. Transaction Patterns
+The demand patterns have implications across multiple functions:
+- Operations: workforce scheduling
+- Marketing: campaign timing
+- IT: system capacity scaling
+
+
+## Visualisation
+![alt text](<Customer Transaction Patterns by Hour and Weekday.png>)
--- retail.py
+++ retail.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv("./data/online_retail_II.csv")
 
@@ -33,7 +35,12 @@ def strutureAndQuality(df):
     ## 1. Data Structure
     print('===EDA'+'='*50)
     print('\n[1] Basic Info')
+    print(df.head())
     print(f'Rows count: {df.shape[0]:,}  Columns count: {df.shape[1]}')
+
+    df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
+    print(f'\nThe first transaction: {df['InvoiceDate'].min()}')
+    print(f'The first transaction: {df['InvoiceDate'].max()}')
 
     ## 2. Data Quality
     print('\n[2] Missing Values')
@@ -105,8 +112,10 @@ def standardise_desc(df):
     """
     Product Description Check, Master data quality issue.
 
-    Issue: Same StockCode is linked to multiple descriptions, e.g. "RETROSPOT" vs "RETRO SPOT"
+    Issue 1: Same StockCode is linked to multiple descriptions, e.g. "RETROSPOT" vs "RETRO SPOT"
     Solution: Standardise description using most frequent value per StockCode   
+
+    Issue 2: Fill missing Customer ID out as 'Guest'
 
     Args:
         df: The raw retail dataframe.
@@ -117,6 +126,8 @@ def standardise_desc(df):
 
     df_clean = df.copy()
 
+
+    ## Multiple Descriptions
     # Uppercase, remove space
     df_clean['Description'] = (df_clean['Description']
                                .astype(str)
@@ -137,9 +148,118 @@ def standardise_desc(df):
     print('Top 15 most frequent descriptions:')
     print(desc_counts.sort_values(by='Count', ascending=False).head(15))
 
+
+    ## Missing Customer ID
+    missing_count = df_clean['Customer ID'].isnull().sum()
+    missing_percent = df_clean['Customer ID'].isnull().mean()*100 # Null=True=1; NotNull=False=0, then average it
+    print(f'Missing ID count: {missing_count} ({missing_percent:.2f} %)')
+    
+    # Fill up the empty customer ID as 'Guest'
+    df_clean['Customer ID'] = df_clean['Customer ID'].fillna('Guest')
+    
+
     return df_clean
 
 
 
+
+def create_features(df_clean):
+    """
+    Create some new features for analysis
+    """
+    df_clean['Revenue'] = df_clean['Quantity'] * df_clean['Price']
+
+
+    # Convert datetime and date feature
+    df_clean['InvoiceDate'] = pd.to_datetime(df_clean['InvoiceDate'])
+    df_clean['Month'] = df_clean['InvoiceDate'].dt.month
+    df_clean['Hour'] = df_clean['InvoiceDate'].dt.hour
+
+    # Weekday order for plot
+    df_clean['DayName'] = df_clean['InvoiceDate'].dt.day_name()
+    weekday_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    df_clean['DayName'] = pd.Categorical(df_clean['DayName'], categories=weekday_order)
+
+    print(f'\nNew features:\n {df_clean.head()}')
+
+    return df_clean
+
+
+def transaction_analysis(df_clean):
+    
+    # 1. Monthly order amount
+    plt.figure(figsize=(12,6))
+    sales_monthly = df_clean.groupby('Month').size()
+    # sns.barplot(x=sales_monthly.index, y=sales_monthly.values)
+    plt.bar(sales_monthly.index, sales_monthly.values, color='tab:olive', width = 0.5, zorder=2)
+    plt.xticks(sales_monthly.index)
+    plt.grid(axis='y', linestyle='--', zorder=0)
+    # plt.bar_label()
+    plt.title('Trading volume by Month')
+    plt.xlabel('Month')
+    plt.ylabel('Number of transactions')
+    plt.show()
+
+    # 2. Weekly order amount
+    plt.figure(figsize=(12,6))
+    sales_weekly = df_clean.groupby('DayName').size()
+    plt.bar(sales_weekly.index, sales_weekly.values, color='tab:green', width = 0.5, zorder=2)
+    plt.xticks(sales_weekly.index)
+    plt.grid(axis='y', linestyle='--', zorder=0)
+    plt.title('Trading volume by Day')
+    plt.xlabel('Day')
+    plt.ylabel('Number of transactions')
+    plt.show()
+
+    # 3. Hourly order amount
+    plt.figure(figsize=(12,6))
+    sales_hour = df_clean.groupby('Hour').size()
+    plt.bar(sales_hour.index, sales_hour.values, color='tab:cyan', width = 0.5, zorder=2)
+    plt.xticks(sales_hour.index)
+    plt.grid(axis='y', linestyle='--', zorder=0)
+    plt.title('Trading volume by per Hour')
+    plt.xlabel('Hours')
+    plt.ylabel('Number of transactions')
+    plt.show()
+
+    # 4. Order amount by hour across weekdays
+    plt.figure(figsize=(12,6))
+
+    # Index: Hour, Columns = Weekday
+    sales_hour_weekday = df_clean.groupby(['Hour', 'DayName']).size().unstack()
+
+    # Dilute the color of weekend
+    color_map = {
+        'Saturday': 'khaki',
+        'Sunday': 'lightgreen'
+    }
+    for day in sales_hour_weekday.columns:
+        if day in ['Saturday', 'Sunday']:
+            plt.plot(sales_hour_weekday.index, sales_hour_weekday[day], marker='o', label=day, color=color_map[day], linestyle='--', linewidth=1)
+        else:
+            plt.plot(sales_hour_weekday.index, sales_hour_weekday[day], marker='o', label=day, linewidth=2)
+
+    plt.xticks(sales_hour_weekday.index)
+    plt.grid(linestyle=':', zorder=0)
+    plt.title('Customer Transaction Patterns by Hour and Weekday')
+    plt.xlabel('Hours')
+    plt.ylabel('Transaction Volume')
+    plt.legend(title='Weekday')
+
+    # Check the abnormal data
+    sat_count = len(df_clean[df_clean['DayName'] == 'Saturday'])
+    sat_percent = sat_count/len(df_clean)*100
+    print(f'Saturday transaction count: {sat_count} ({sat_percent:.2f}%)')
+    # Annotation
+    anno_text = f'**Note: Saturday accounts for only {sat_percent:.2f}% of all transactions. \n May indicate store closure or system data loss.'
+    plt.text(x=0.3, y=0.02, s=anno_text, transform=plt.gcf().transFigure)
+
+    plt.tight_layout() 
+    plt.subplots_adjust(bottom=0.18)
+    plt.show()
+
+
+strutureAndQuality(df)
 df_clean = standardise_desc(df)
-# strutureAndQuality(df)
+df_clean = create_features(df_clean)
+transaction_analysis(df_clean)
